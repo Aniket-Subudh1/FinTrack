a49cb8fdb7b25b5bfe5082d29b90af2862cbf658
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2024-10-02 22:44:46 +0530

    Error Fixing

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @Component({
