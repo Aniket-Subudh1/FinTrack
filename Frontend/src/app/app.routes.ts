6164566a5ea276acdda415acc3490c6af5ef0b45
Durga1234-cloud <puhandurgasankarp@gmail.com>
2024-10-02 23:28:01 +0530

    sign up update

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component'; 
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
+    { path: 'signup', component: SignUpComponent },
+    {path:'',component:HomeComponent }
   
 ];
