3dea1048f03f4f4db8450056f1d7080bfb7178fe
Durga1234-cloud <puhandurgasankarp@gmail.com>
2024-10-03 00:14:46 +0530

    modify login

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './pages/signup/signup.component'; 
+import { LoginComponent  } from './pages/login/login.component'; 
 import { HomeComponent} from './pages/home/home.component'; 
+
 
 
 
 export const routes: Routes = [
     { path: 'signup', component: SignUpComponent },
+    { path: 'login', component:LoginComponent  },
     { path: '', component:  HomeComponent },
    
   
