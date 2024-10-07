468d877e7b4c14f19d2e8098d2d5203da28f5c70
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2024-10-07 23:35:37 +0530

    Minor change

--- Frontend/src/app/homeContainer/section1/section1.component.ts
+++ Frontend/src/app/homeContainer/section1/section1.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-section1',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './section1.component.html',
   styleUrl: './section1.component.css'
 })
+
 export class Section1Component {
+  constructor (private router: Router) {
+    
+  }
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+  
 
 }
