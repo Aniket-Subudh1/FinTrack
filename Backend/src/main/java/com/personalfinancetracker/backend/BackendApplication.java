d6b70f4331d8158fa0ef8cceeeb2351f54f18a2b
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2024-10-09 23:59:51 +0530

    minor change

--- Backend/src/main/java/com/personalfinancetracker/backend/BackendApplication.java
+++ Backend/src/main/java/com/personalfinancetracker/backend/BackendApplication.java
@@ -1,6 +1,6 @@
 package com.personalfinancetracker.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,13 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        // Load environment variables from .env file
-        Dotenv dotenv = Dotenv.load();
-
-        // Optionally, you can access specific environment variables for debugging purposes
-        System.out.println("DB Username: " + dotenv.get("DB_USERNAME"));
-        System.out.println("Google Client ID: " + dotenv.get("GOOGLE_CLIENT_ID"));
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
