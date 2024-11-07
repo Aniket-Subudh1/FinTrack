788d66f00acca9c6693699466418b8af354b65d1
BIBH123 <bibhuprasadpradhan82@gmail.com>
2024-11-07 13:43:05 +0530

    add crete category frotend

--- Backend/src/main/java/com/personalfinancetracker/backend/repository/IncomeRepository.java
+++ Backend/src/main/java/com/personalfinancetracker/backend/repository/IncomeRepository.java
@@ -1,9 +1,17 @@
 package com.personalfinancetracker.backend.repository;
 
+import com.personalfinancetracker.backend.entities.Expense;
 import com.personalfinancetracker.backend.entities.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 
-public interface IncomeRepository extends JpaRepository<Income, Long> {
+public interface IncomeRepository extends JpaRepository<Expense, Long> {
+
+//    List<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate);
+
 }
