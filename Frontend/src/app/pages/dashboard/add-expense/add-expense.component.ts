3cdc10b9b497c77f48a025451f4cc210d35a8b11
Durga1234-cloud <puhandurgasankarp@gmail.com>
2024-11-16 18:46:39 +0530

    modify Add Expense frontend

--- Frontend/src/app/pages/dashboard/add-expense/add-expense.component.ts
+++ Frontend/src/app/pages/dashboard/add-expense/add-expense.component.ts
@@ -1,7 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
 import { ExpenseService, Expense } from '../../../service/expense.service';
-import { CustomerService } from '../../../service/customer.service';// Import the customer service
-import { NgIf, NgFor } from '@angular/common'; // Import NgFor
+import { NgIf, NgFor } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SidebarComponent } from '../sidebar/sidebar.component';
@@ -11,34 +11,20 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
   standalone: true,
   templateUrl: './add-expense.component.html',
   styleUrls: ['./add-expense.component.css'],
-  imports: [NgIf, NgFor, FormsModule, HttpClientModule, SidebarComponent] // Import NgFor here
- // Import NgFor here
+  imports: [NgIf, NgFor, FormsModule, HttpClientModule, SidebarComponent]
 })
 export class AddExpenseComponent implements OnInit {
   expense: Expense = {
-    userName: '',
     amount: 0,
-    category: ''
+    category: '',
+    userName: ''
   };
 
   successMessage: string = '';
-  customers: string[] = []; // Store the fetched customer names
 
-  constructor(
-    private expenseService: ExpenseService,
-    private customerService: CustomerService // Inject the CustomerService
-  ) {}
+  constructor(private expenseService: ExpenseService) {}
 
-  ngOnInit() {
-    this.customerService.getAllCustomerNames().subscribe(
-      (customerNames) => {
-        this.customers = customerNames; // Populate the customers array with fetched names
-      },
-      (error) => {
-        console.error('Error fetching customer names', error);
-      }
-    );
-  }
+  ngOnInit() {}
 
   onSubmit(form: NgForm) {
     if (form.valid) {
